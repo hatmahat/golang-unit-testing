91d8a4aaa16e3dbad24fccede4fe9969fd7606d2
Mahatma <hatmahat@gmail.com>
2022-07-13 11:54:28 +0700

    last commit (#5)

--- calc_test.go
+++ calc_test.go
@@ -1,48 +1,97 @@
 package main
 
 import (
-	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestCalculator_Add(t *testing.T) {
-	// t.Run("Calculator Add Operator Testing", func(t *testing.T) {
-	// 	calc := Calculator{
-	// 		Num1: 2,
-	// 		Num2: 3,
-	// 	}
-	// 	expected := 5
-	// 	actual := calc.Add()
-	// 	if expected != actual {
-	// 		t.Error("The result should be", expected)
-	// 	}
-	// })
-	myCalc := Calculator{Num1: 4, Num2: 4}
-	assert.Equal(t, 8, myCalc.Add(), "it should return 8")
+type CalculatorTestSuite struct {
+	suite.Suite
+	calcService Calculator
 }
 
-func TestCalculator_Sub(t *testing.T) {
-	// t.Run("Calculator Substraction operator testing", func(t *testing.T) {
-	// 	calc := Calculator{
-	// 		Num1: 2,
-	// 		Num2: 3,
-	// 	}
-	// 	expected := -1
-	// 	actual := calc.Sub()
-	// 	if expected != actual {
-	// 		t.Error("The result should be", expected)
-	// 	}
-	// })
-	myCalc := Calculator{Num1: 5, Num2: 4}
-	assert.Equal(t, 1, myCalc.Sub(), "it should return 1")
+func (suite *CalculatorTestSuite) SetupTest() {
+	suite.calcService = Calculator{}
 }
 
-func TestMainApp_Add(t *testing.T) {
-	os.Args = []string{"test", "-num1", "2", "-num2", "3", "-opr", "add"}
-	main()
+func (suite *CalculatorTestSuite) TestCalculator_Add() {
+	testTable := []struct {
+		num1 int
+		num2 int
+		res  int
+	}{
+		{4, 4, 8},
+		{-4, 5, -1},
+	}
+	for _, table := range testTable {
+		suite.calcService.Num1 = table.num1
+		suite.calcService.Num2 = table.num2
+		actual, err := suite.calcService.Add()
+		if err != nil {
+			assert.NotNil(suite.T(), err)
+			assert.Equal(suite.T(), table.res, actual)
+		} else {
+			assert.Nil(suite.T(), err)
+			assert.Equal(suite.T(), table.res, actual)
+		}
+	}
+	// suite.calcService.Num1 = 4
+	// suite.calcService.Num2 = 4
+	// actual, err := suite.calcService.Add()
+	// expected := 8
+	// assert.Equal(suite.T(), expected, actual, "it should return 8")
 }
+
+func (suite *CalculatorTestSuite) TestCalculator_Sub() {
+	suite.calcService.Num1 = 5
+	suite.calcService.Num2 = 4
+	actual := suite.calcService.Sub()
+	expected := 1
+	assert.Equal(suite.T(), expected, actual, "it should return 1")
+}
+
+func TestCalculatorTestSuite(t *testing.T) {
+	suite.Run(t, new(CalculatorTestSuite))
+}
+
+// func TestCalculator_Add(t *testing.T) {
+// 	// t.Run("Calculator Add Operator Testing", func(t *testing.T) {
+// 	// 	calc := Calculator{
+// 	// 		Num1: 2,
+// 	// 		Num2: 3,
+// 	// 	}
+// 	// 	expected := 5
+// 	// 	actual := calc.Add()
+// 	// 	if expected != actual {
+// 	// 		t.Error("The result should be", expected)
+// 	// 	}
+// 	// })
+// 	myCalc := Calculator{Num1: 4, Num2: 4}
+// 	assert.Equal(t, 8, myCalc.Add(), "it should return 8")
+// }
+
+// func TestCalculator_Sub(t *testing.T) {
+// 	// t.Run("Calculator Substraction operator testing", func(t *testing.T) {
+// 	// 	calc := Calculator{
+// 	// 		Num1: 2,
+// 	// 		Num2: 3,
+// 	// 	}
+// 	// 	expected := -1
+// 	// 	actual := calc.Sub()
+// 	// 	if expected != actual {
+// 	// 		t.Error("The result should be", expected)
+// 	// 	}
+// 	// })
+// 	myCalc := Calculator{Num1: 5, Num2: 4}
+// 	assert.Equal(t, 1, myCalc.Sub(), "it should return 1")
+// }
+
+// func TestMainApp_Add(t *testing.T) {
+// 	os.Args = []string{"test", "-num1", "2", "-num2", "3", "-opr", "add"}
+// 	main()
+// }
 
 /*
 go test
